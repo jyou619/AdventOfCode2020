cc3cac4f0438e32d41b56b64cd441793673b0583
Joshua You <jyou619@gmail.com>
2020-12-24 21:25:04 +1000

    finished day 7

--- day7/day7.py
+++ day7/day7.py
@@ -14,6 +14,21 @@ for line in f:
     while i < len(line) and line[i] != 'no' :
         count = int(line[i])
         child = line[i + 1] + line[i + 2]
-        contains.append((child, count))
+        contains.append((count, child))
         i += 4
     rules[bag] = contains
+
+G = nx.DiGraph()
+for bag, contains in rules.items():
+    for _, child in contains:
+        G.add_edge(child, bag)
+
+def get_bags(type):
+    i = 0
+    for count, child in rules[type]:
+        i += count * get_bags(child)
+    return i + 1
+
+print(len(nx.predecessor(G, 'shinygold')) - 1)
+
+print(get_bags('shinygold') - 1)
